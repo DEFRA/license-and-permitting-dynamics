ae0e88dc51b0b34321a9d68876f9c6d796376426
joshgranteaproject <joshuagranteaproject@gmail.com>
2017-10-19 16:01:12 +0100

    Pushing the updated address lookup

--- Crm/LicensingandPermitting/Defra.Lp/Solution/WebResources/defra_/addresslookup/postcodeOnchange.js
+++ Crm/LicensingandPermitting/Defra.Lp/Solution/WebResources/defra_/addresslookup/postcodeOnchange.js
@@ -1,4 +1,8 @@
 function postcodeOnChange() {
-    Xrm.Page.getControl("WebResource_address").setSrc(Xrm.Page.getControl("WebResource_address").getSrc());
+   var addressWebResource = Xrm.Page.getControl("WebResource_address");
+   var src = addressWebResource.getSrc();
+   addressWebResource .setSrc(null)
+   addressWebResource .setSrc(src);
+  //  Xrm.Page.getControl("WebResource_address").setSrc(Xrm.Page.getControl("WebResource_address").getSrc());
     Xrm.Page.getControl("WebResource_address").setVisible(true);
 }
