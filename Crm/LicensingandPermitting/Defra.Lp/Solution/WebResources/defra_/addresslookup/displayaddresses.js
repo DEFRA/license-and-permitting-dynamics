c974f5e9a05e439d33c1058de3c1e0482318adfd
Chris Jibb <christopher.jibb@civica.co.uk>
2017-10-27 09:54:13 +0100

    Updating branch with lastest version from CRM

--- Crm/LicensingandPermitting/Defra.Lp/Solution/WebResources/defra_/addresslookup/displayaddresses.js
+++ Crm/LicensingandPermitting/Defra.Lp/Solution/WebResources/defra_/addresslookup/displayaddresses.js
@@ -1,31 +1,91 @@
 function displayAddresses() {
-    var addressses = { "totalMatches": 3, "startMatch": 1, "endMatch": 3, "uri_to_supplier": "https://api.ordnancesurvey.co.uk/places/v1/addresses/postcode?lr=EN&postcode=bs15ah&maxresults=100&dataset=DPA", "uri_from_client": "stub", "results": [{ "uprn": 340116, "address": "ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH", "organisation": "ENVIRONMENT AGENCY", "premises": "HORIZON HOUSE", "street_address": "DEANERY ROAD", "locality": null, "city": "BRISTOL", "postcode": "BS1 5AH", "x": "358205.03", "y": "172708.07", "coordinate_system": null, "state_date": "12/10/2009", "blpu_state_code": null, "postal_address_code": null, "logical_status_code": null, "source_data_type": "dpa" }, { "uprn": 10091760640, "address": "HARMSEN GROUP, TRIODOS BANK, DEANERY ROAD, BRISTOL, BS1 5AH", "organisation": "HARMSEN GROUP", "premises": "TRIODOS BANK", "street_address": "DEANERY ROAD", "locality": null, "city": "BRISTOL", "postcode": "BS1 5AH", "x": "358130.1", "y": "172687.87", "coordinate_system": null, "state_date": null, "blpu_state_code": null, "postal_address_code": null, "logical_status_code": null, "source_data_type": "dpa" }, { "uprn": 340117, "address": "THRIVE RENEWABLES PLC, DEANERY ROAD, BRISTOL, BS1 5AH", "organisation": "THRIVE RENEWABLES PLC", "premises": null, "street_address": "DEANERY ROAD", "locality": null, "city": "BRISTOL", "postcode": "BS1 5AH", "x": "358130.1", "y": "172687.87", "coordinate_system": null, "state_date": "12/10/2009", "blpu_state_code": null, "postal_address_code": null, "logical_status_code": null, "source_data_type": "dpa" }] };
-    // var postcode = Xrm.Page.getControl("defra_postcode").getValue();
-    // var addressbaseAPI = "https://addressfacade.cloudapp.net/address-service/v1/addresses/postcode?key=client1&postcode=gu227uy";
-    // $.getJSON(addressbaseAPI)
-    //   .done(function (data) {
-    //       $.each(data.items, function (i, item) {
-    //           jQuery('#the_address').append(
-    //           jQuery('<option></option').val(i).html(item)
-    //       )
-    //       });
-    //   });
-    jQuery('#the_address').empty();
-    jQuery('#the_address').append(jQuery('<option></option').val(0).html(addressses.totalMatches + " addresses found"));
-    $.each(addressses.results, function (i, item) {
-        jQuery('#the_address').append(
-            jQuery('<option></option').val(i).html(item.address)
-        )
-    });
-    jQuery('select[name="siteAddress"]').change(function () {
-        var addressSelected = jQuery("#the_address option:selected").text();
-        
-        var addressSeperated = addressSelected.split(',');
- 
+    var uri = Xrm.Page.context.getClientUrl() + "/api/data/v8.2/defra_postcodelookup";
+    //encodeURI(uri+"defra_postcodelookup");
 
-        window.parent.Xrm.Page.getAttribute("defra_name").setValue(addressSelected);
-        window.parent.Xrm.Page.getAttribute("defra_premises").setValue(addressSeperated[1]);
-        window.parent.Xrm.Page.getAttribute("defra_street").setValue(addressSeperated[2]);
-    });
+
+    jQuery('#the_address').empty();
+    var postcode = window.parent.Xrm.Page.getControl("defra_postcode").getValue();
+    var requestdata = JSON.stringify({ "postcode": postcode });
+
+    if (postcode === "") {
+
+    } else {
+        /*  var addressbaseAPI = "https://addressfacade.cloudapp.net/address-service/v1/addresses/postcode?key=client1&postcode=gu227uy"; */
+
+        $.ajax({
+            url: uri,
+            type: 'POST',
+            data: requestdata,
+            dataType: 'json',
+            contentType: "application/json",
+            async: false,
+            success: function (data) {
+
+                var addresses = JSON.parse(data.addresses);
+
+                jQuery('#the_address').append(jQuery('<option></option').val(0).html(addresses.totalMatches + " addresses found"));
+                window.parent.Xrm.Page.ui.clearFormNotification("noAddress");
+                $.each(addresses.results, function (i, results) {
+                    jQuery('#the_address').append(
+                        jQuery('<option></option').val(i).html(results.address)
+
+                    )
+
+                });
+
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+
+                var responseText = JSON.stringify(jqXHR.responseText);
+                //var responseText = responseText.slice(161, 255)
+                if (jqXHR.status == 404) {
+                    window.parent.Xrm.Page.ui.setFormNotification("The URL requested wasn't valid. The server responded with a 404 error", "ERROR", "noAddress");
+                }
+                if (jqXHR.status == 500) {
+                    window.parent.Xrm.Page.ui.setFormNotification("Invalid postcode entered. The postcode must contain a minimum of the sector plus 1 digit of the district e.g. SO1", "ERROR", "noAddress");
+                }
+            }
+        });
+
+
+        jQuery('select[name="siteAddress"]').change(function () {
+            //jQuery("#the_address").children().first().remove();
+
+
+            var addressSelected = jQuery("#the_address option:selected").text();
+
+
+             $.ajax({
+            url: uri,
+            type: 'POST',
+            data: requestdata,
+            dataType: 'json',
+            contentType: "application/json",
+            async: false,
+            success: function (data) {
+
+                var addresses = JSON.parse(data.addresses);
+
+                $.each(addresses.results, function (i, results) {
+                    if(results.address == addressSelected)
+                    {
+                      window.parent.Xrm.Page.getAttribute("defra_premises").setValue(results.premises);   
+                      window.parent.Xrm.Page.getAttribute("defra_street").setValue(results.street_address);
+                      window.parent.Xrm.Page.getAttribute("defra_locality").setValue(results.locality);
+                    }
+
+                    
+
+                });
+
+
+            }
+    
+        });
+            window.parent.Xrm.Page.getAttribute("defra_name").setValue(addressSelected);
+
+        });
+    }
 }
 
