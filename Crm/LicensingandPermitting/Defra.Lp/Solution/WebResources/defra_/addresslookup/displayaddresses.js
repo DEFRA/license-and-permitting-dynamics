d1f886d8ca169bd4f9adc6c9c94a94a1ad96d997
Chris Jibb <christopher.jibb@civica.co.uk>
2017-11-08 15:10:45 +0000

    Updating with latest L&P solution from CRM following Kal's updates

--- Crm/LicensingandPermitting/Defra.Lp/Solution/WebResources/defra_/addresslookup/displayaddresses.js
+++ Crm/LicensingandPermitting/Defra.Lp/Solution/WebResources/defra_/addresslookup/displayaddresses.js
@@ -1,21 +1,32 @@
+var addresses = null;
+
 function displayAddresses() {
     var uri = Xrm.Page.context.getClientUrl() + "/api/data/v8.2/defra_postcodelookup";
-    //encodeURI(uri+"defra_postcodelookup");
-            window.parent.Xrm.Page.getControl("defra_name").setDisabled(false);
-            window.parent.Xrm.Page.getControl("defra_premises").setDisabled(false);
-            window.parent.Xrm.Page.getControl("defra_street").setDisabled(false);
-            window.parent.Xrm.Page.getControl("defra_locality").setDisabled(false);
-            window.parent.Xrm.Page.getControl("defra_towntext").setDisabled(false);
 
-    jQuery('#the_address').empty();
-    var postcode = window.parent.Xrm.Page.getControl("defra_postcode").getValue();
+    window.parent.Xrm.Page.ui.clearFormNotification("noAddress");
+	
+	ClearAll();
+	
+	window.parent.Xrm.Page.getAttribute("defra_fromaddresslookup").setValue(false);
+	window.parent.Xrm.Page.getAttribute("defra_fromaddresslookup").fireOnChange();
+	
+	jQuery('#the_address').hide();
+	jQuery('#the_address').empty();
+	jQuery('#lkp_msg').html("");
+	
+    var postcode = window.parent.Xrm.Page.getAttribute("defra_postcode").getValue();
+	
+	if(postcode == null){
+		return;
+	}
+	
     var requestdata = JSON.stringify({ "postcode": postcode });
-
+				
     if (postcode === "") {
 
     } else {
-        /*  var addressbaseAPI = "https://addressfacade.cloudapp.net/address-service/v1/addresses/postcode?key=client1&postcode=gu227uy"; */
-
+		jQuery('#lkp_msg').html("Retrieving postcode addresses");
+		
         $.ajax({
             url: uri,
             type: 'POST',
@@ -24,24 +35,32 @@ function displayAddresses() {
             contentType: "application/json",
             async: false,
             success: function (data) {
+			
+				jQuery('#lkp_msg').html("");
+                
+				addresses = JSON.parse(data.addresses);
+				
+				if(addresses.totalMatches > 0) {
+					jQuery('#lkp_msg').html(addresses.totalMatches + " addresses found");
+					
+					jQuery('#the_address').append(jQuery('<option></option').val(0).html("Please select an address"));
 
-                var addresses = JSON.parse(data.addresses);
+					$.each(addresses.results, function (i, results) {
+						jQuery('#the_address').append(
+							jQuery('<option></option').val(i).html(results.address)
 
-                jQuery('#the_address').append(jQuery('<option></option').val(0).html(addresses.totalMatches + " addresses found"));
-                window.parent.Xrm.Page.ui.clearFormNotification("noAddress");
-                $.each(addresses.results, function (i, results) {
-                    jQuery('#the_address').append(
-                        jQuery('<option></option').val(i).html(results.address)
+						)
 
-                    )
-
-                });
-
-
+					});
+					
+					jQuery('#the_address').show();
+				} 
             },
             error: function (jqXHR, textStatus, errorThrown) {
-
-                var responseText = JSON.stringify(jqXHR.responseText);
+				
+				jQuery('#lkp_msg').html("");
+                
+				var responseText = JSON.stringify(jqXHR.responseText);
                 //var responseText = responseText.slice(161, 255)
                 if (jqXHR.status == 404) {
                     window.parent.Xrm.Page.ui.setFormNotification("The URL requested wasn't valid. The server responded with a 404 error", "ERROR", "noAddress");
@@ -54,48 +73,40 @@ function displayAddresses() {
 
 
         jQuery('select[name="siteAddress"]').change(function () {
-            //jQuery("#the_address").children().first().remove();
+            //Check if it is the first option
+			if($("#the_address")[0].selectedIndex == 0){
+				ClearAll();
+				
+				window.parent.Xrm.Page.getAttribute("defra_fromaddresslookup").setValue(false);
+				window.parent.Xrm.Page.getAttribute("defra_fromaddresslookup").fireOnChange();
+	
+				return;
+			}
 
 
             var addressSelected = jQuery("#the_address option:selected").text();
+			window.parent.Xrm.Page.getAttribute("defra_name").setValue(addressSelected);
+            
+			$.each(addresses.results, function (i, results) {
+				if(results.address == addressSelected)
+                {
+					window.parent.Xrm.Page.getAttribute("defra_premises").setValue(results.premises);   
+                    window.parent.Xrm.Page.getAttribute("defra_street").setValue(results.street_address);
+                    window.parent.Xrm.Page.getAttribute("defra_locality").setValue(results.locality);
+                    window.parent.Xrm.Page.getAttribute("defra_towntext").setValue(results.city);
+                }
+			});
 
-
-             $.ajax({
-            url: uri,
-            type: 'POST',
-            data: requestdata,
-            dataType: 'json',
-            contentType: "application/json",
-            async: false,
-            success: function (data) {
-
-                var addresses = JSON.parse(data.addresses);
-
-                $.each(addresses.results, function (i, results) {
-                    if(results.address == addressSelected)
-                    {
-                      window.parent.Xrm.Page.getAttribute("defra_premises").setValue(results.premises);   
-                      window.parent.Xrm.Page.getAttribute("defra_street").setValue(results.street_address);
-                      window.parent.Xrm.Page.getAttribute("defra_locality").setValue(results.locality);
-                      window.parent.Xrm.Page.getAttribute("defra_towntext").setValue(results.city);
-                    }
-
-                    
-
-                });
-
-
-            }
-    
-        });
-            window.parent.Xrm.Page.getControl("defra_name").setDisabled(true);
-            window.parent.Xrm.Page.getControl("defra_premises").setDisabled(true);
-            window.parent.Xrm.Page.getControl("defra_street").setDisabled(true);
-            window.parent.Xrm.Page.getControl("defra_locality").setDisabled(true);
-            window.parent.Xrm.Page.getControl("defra_towntext").setDisabled(true);
-            window.parent.Xrm.Page.getAttribute("defra_name").setValue(addressSelected);
-
+			window.parent.Xrm.Page.getAttribute("defra_fromaddresslookup").setValue(true);
+			window.parent.Xrm.Page.getAttribute("defra_fromaddresslookup").fireOnChange();
         });
     }
 }
 
+function ClearAll(){
+	window.parent.Xrm.Page.getAttribute("defra_name").setValue(null);
+	window.parent.Xrm.Page.getAttribute("defra_premises").setValue(null);   
+	window.parent.Xrm.Page.getAttribute("defra_street").setValue(null);
+    window.parent.Xrm.Page.getAttribute("defra_locality").setValue(null);
+    window.parent.Xrm.Page.getAttribute("defra_towntext").setValue(null);
+}
