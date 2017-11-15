edfaa448da6892c68c77512552f49312e8d0ae45
Chris Jibb <christopher.jibb@civica.co.uk>
2017-11-15 11:42:49 +0000

    Adding OwnershipManager Plugin

---
+++ Crm/WastePermits/Defra.Lp.WastePermits/Plugins/Common/OwnershipManager.cs
@@ -0,0 +1,127 @@
+﻿namespace Defra.Lp.WastePermits.Plugins.Common.Security
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Query;
+    using Microsoft.Crm.Sdk.Messages;
+
+    /// <summary>
+    /// Recprds Ownership Management Class
+    /// </summary>
+    public class OwnershipManager : OwnershipManagerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OwnershipManager"/> class.
+        /// </summary>
+        /// <param name="Service">The service.</param>
+        /// <param name="Context">The context.</param>
+        /// <param name="preImage">The pre image.</param>
+        public OwnershipManager(IOrganizationService Service, IPluginExecutionContext Context, Entity preImage)
+            : base(Service, Context, preImage)
+        {
+        }
+
+        /// <summary>
+        /// Sets the owner.
+        /// </summary>
+        public void SetOwner()
+        {
+            //Manages the owner on record creation
+            if (this._Context.MessageName == "Create")
+                this.SetOwnerCreate();
+
+            if (this._Context.MessageName == "Update")
+                this.SetOwnerUpdate();
+        }
+
+        /// <summary>
+        /// Sets the owner create.
+        /// </summary>
+        private void SetOwnerCreate()
+        {
+            Guid? teamId = this.GetOwnerTeam();
+
+            if (teamId.HasValue && teamId.Value != Guid.Empty)
+                this.AssignTheRecord(teamId.Value);
+        }
+
+        /// <summary>
+        /// Gets the owner team.
+        /// </summary>
+        /// <returns></returns>
+        private Guid? GetOwnerTeam()
+        {
+            var query = new QueryExpression("team")
+            {
+                ColumnSet = new ColumnSet("teamid"),
+
+                Criteria = new FilterExpression(LogicalOperator.And)
+                {
+                    Conditions =
+          {
+            new ConditionExpression("businessunitid", ConditionOperator.Equal, this._Context.BusinessUnitId),
+            new ConditionExpression("name", ConditionOperator.Like, "% Owner")
+          }
+                }
+            };
+
+            EntityCollection results = _Service.RetrieveMultiple(query);
+
+            if (results.Entities.Count > 0)
+                return results.Entities[0].Id;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the owner on update.
+        /// </summary>
+        private void SetOwnerUpdate()
+        {
+            // Enforcing Cascading security due to platform issue to be investigated before adding defra_examples to this method
+            //switch (Target.LogicalName)
+            //{
+            //    case "defra_example":
+            //        if (Target.Attributes.Contains("defra_exampleid") && Target.Attributes["defra_exampleid"] != null)
+            //            this.Reparent((EntityReference)Target.Attributes["defra_exampleid"]);
+            //        break;
+            //    default:
+
+            //        break;
+            //}
+        }
+
+        private void Reparent(EntityReference parentER)
+        {
+            Entity parent = _Service.Retrieve(parentER.LogicalName, parentER.Id, new ColumnSet("ownerid"));
+
+            if (parent.Attributes.Contains("ownerid"))
+            {
+                AssignRequest assignReq = new AssignRequest()
+                {
+                    Assignee = (EntityReference)parent.Attributes["ownerid"],
+                    Target = new EntityReference(_Context.PrimaryEntityName, _Context.PrimaryEntityId)
+                };
+
+                _Service.Execute(assignReq);
+            }
+        }
+
+        /// <summary>
+        /// Assigns the record.
+        /// </summary>
+        /// <param name="teamid">The teamid.</param>
+        private void AssignTheRecord(Guid teamid)
+        {
+            if (teamid != Guid.Empty && _Context.InputParameters.Contains("Target") && _Context.InputParameters["Target"] is Entity)
+            {
+                Entity target = (Entity)_Context.InputParameters["Target"];
+
+                if (target.Attributes.Contains("ownerid"))
+                    target.Attributes["ownerid"] = new EntityReference("team", teamid);
+                else
+                    target.Attributes.Add("ownerid", new EntityReference("team", teamid));
+            }
+        }
+    }
+}
---
+++ Crm/WastePermits/Defra.Lp.WastePermits/Plugins/Common/OwnershipManagerBase.cs
@@ -0,0 +1,54 @@
+﻿namespace Defra.Lp.WastePermits.Plugins.Common.Security
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+
+    /// <summary>
+    /// Record Ownership Base Class
+    /// </summary>
+    public abstract class OwnershipManagerBase
+    {
+        /// <summary>
+        /// Gets or sets the CRM Service.
+        /// </summary>
+        /// <value>
+        /// The _ service.
+        /// </value>
+        protected IOrganizationService _Service { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Plug in Context.
+        /// </summary>
+        /// <value>
+        /// The _ context.
+        /// </value>
+        protected IPluginExecutionContext _Context { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pre image.
+        /// </summary>
+        /// <value>
+        /// The pre image.
+        /// </value>
+        protected Entity preImage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target.
+        /// </summary>
+        /// <value>
+        /// The target.
+        /// </value>
+        protected Entity Target { get; set; }
+
+        public OwnershipManagerBase(IOrganizationService Service, IPluginExecutionContext Context, Entity preImage)
+        {
+            this._Service = Service;
+            this._Context = Context;
+            this.preImage = preImage;
+
+            //Target cast
+            if (this._Context.InputParameters.Contains("Target") && this._Context.InputParameters["Target"] is Entity)
+                this.Target = (Entity)this._Context.InputParameters["Target"];
+        }
+    }
+}
