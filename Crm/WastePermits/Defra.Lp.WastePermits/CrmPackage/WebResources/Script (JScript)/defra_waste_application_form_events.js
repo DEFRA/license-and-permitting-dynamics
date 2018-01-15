472395cb0a1130f968eed4e28f7856e8f8f14737
Chris Jibb <christopher.jibb@civica.co.uk>
2018-01-15 22:05:14 +0000

    Various screening and duly made changes

--- Crm/WastePermits/Defra.Lp.WastePermits/CrmPackage/WebResources/Script (JScript)/defra_waste_application_form_events.js
+++ Crm/WastePermits/Defra.Lp.WastePermits/CrmPackage/WebResources/Script (JScript)/defra_waste_application_form_events.js
@@ -1,4 +1,5 @@
 var paymentNotificationID = 'PaymentReceivedWARNING';
+var dulymadechecklistNotificationID = 'DulymadeChecklistWARNING';
 
 function PaymentReceived() {
     if (Xrm.Page.ui.getFormType() != 1) {
@@ -8,6 +9,15 @@ function PaymentReceived() {
             Xrm.Page.ui.setFormNotification('The Application is not paid in full!', 'WARNING', paymentNotificationID);
         else
             Xrm.Page.ui.clearFormNotification(paymentNotificationID);
+
+        // Show notification if Duly Made Checklist not completed but intelligence checks have been completed.
+        //var completedDulyMade = Xrm.Page.getAttribute('defra_completeddulymade');
+        //var raguCompleted = Xrm.Page.getAttribute('defra_raguscore');
+        //if (raguCompleted != null) {
+        //    if (completedDulyMade != null && completedDulyMade.getValue() == false && raguCompleted.getValue() != null)
+        //        Xrm.Page.ui.setFormNotification('The Duly Made Checklist still needs to be completed!', 'WARNING', dulymadechecklistNotificationID);
+        //    else
+        //        Xrm.Page.ui.clearFormNotification(dulymadechecklistNotificationID);
+        //}
     }
 }
-
