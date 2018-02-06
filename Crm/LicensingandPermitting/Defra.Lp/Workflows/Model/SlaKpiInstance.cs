2ac58dbb78e44fdbc84ed6ff4297269a6201b79a
Hugo Herrera (Defra) <hugo.herrera.defra@gmail.com>
2018-02-06 15:40:39 +0000

    Moved two workflow code activities from Core to L&P assembly. L&P now includes the RecalculateRollup and SetSlaKpiDate activities.

---
+++ Crm/LicensingandPermitting/Defra.Lp/Workflows/Model/SlaKpiInstance.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="SlaKpiInstance.cs" company="">
+// Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author></author>
+// <date>05/02/2018</date>
+// <summary>Model for the SlaKpiInstance entity</summary>
+namespace Defra.Lp.Workflows.Model
+{
+    /// <summary>
+    /// Main Model Class
+    /// </summary>
+    internal class SlaKpiInstance
+    {
+        public const string LogicalName = "slakpiinstance";
+        public const string FailureTime = "failuretime";
+        public const string WarningTime = "warningtime";
+        public const string ComputedFailureTime = "computedfailuretime";
+        public const string ComputedWarningTime = "computedwarningtime";
+    }
+}
