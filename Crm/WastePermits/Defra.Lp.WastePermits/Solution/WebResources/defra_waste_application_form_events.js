0d50706453ad16de72c766c609824f4e9d55bb52
Hugo Herrera <hugo.herrera.defra@gmail.com>
2018-02-19 22:12:15 +0000

    Fix for WE-794, added IntelligenceCheck JS function to defra_waste_application_form_events.js, also includes refresh of Waste CRM solution that includes the udpated JS function and update to workflow that processes assignments when Application RAGU score changes.

--- Crm/WastePermits/Defra.Lp.WastePermits/Solution/WebResources/defra_waste_application_form_events.js
+++ Crm/WastePermits/Defra.Lp.WastePermits/Solution/WebResources/defra_waste_application_form_events.js
@@ -1,5 +1,8 @@
 var paymentNotificationID = 'PaymentReceivedWARNING';
 var dulymadechecklistNotificationID = 'DulymadeChecklistWARNING';
+var intelligenceCheckNotificationID = 'IntelligenceCheckWARNING';
+var raguRed = 910400000;
+var raguAmber = 910400001;
 
 function PaymentReceived() {
     if (Xrm.Page.ui.getFormType() != 1) {
@@ -19,5 +22,21 @@ function PaymentReceived() {
         //    else
         //        Xrm.Page.ui.clearFormNotification(dulymadechecklistNotificationID);
         //}
+    }
+}
+
+function IntelligenceCheck() {
+    if (Xrm.Page.ui.getFormType() != 1) {
+        var raguScore = Xrm.Page.getAttribute('defra_raguscore');
+
+
+        if (raguScore != null && (raguScore.getValue() === raguRed || raguScore.getValue() === raguAmber)) 
+        {
+            Xrm.Page.ui.setFormNotification('RAGU Score is ' + raguScore.getText(), 'WARNING', intelligenceCheckNotificationID);
+        }  
+        else
+        {
+            Xrm.Page.ui.clearFormNotification(intelligenceCheckNotificationID);
+        }
     }
 }
