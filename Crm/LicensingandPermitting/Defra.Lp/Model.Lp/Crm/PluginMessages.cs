c1abcd82c4d820fdbbea17adde7fba275c76c9f3
Hugo Herrera <hugo_c_herrera@msn.com>
2018-02-26 14:06:10 +0000

    WE-947 - Fix for error introduced in previous update, account form now refreshes after successful or failed Companies House lookup.

---
+++ Crm/LicensingandPermitting/Defra.Lp/Model.Lp/Crm/PluginMessages.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Model.Lp.Crm
+{
+    /// <summary>
+    /// Messages that may trigger an plugin
+    /// </summary>
+    public static class PluginMessages
+    {
+        public const string Create = "Create";
+        public const string Update = "Update";
+        public const string Delete = "Delete";
+        public const string SetState = "SetState";
+        public const string SetStateDynamicEntity = "SetStateDynamicEntity";
+    }
+}
---
+++ Crm/WastePermits/Defra.Lp.WastePermits/Plugins/Common/ApplicationManager.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace Defra.Lp.WastePermits.Plugins.Common
+{
+    using System;
+    using Security;
+    using Microsoft.Crm.Sdk.Messages;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Query;
+
+    /// <summary>
+    /// Recprds Ownership Management Class
+    /// </summary>
+    public class ApplicationManager : OwnershipManagerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OwnershipManager"/> class.
+        /// </summary>
+        /// <param name="Service">The service.</param>
+        /// <param name="Context">The context.</param>
+        /// <param name="preImage">The pre image.</param>
+        public ApplicationManager(IOrganizationService Service, IPluginExecutionContext Context, Entity preImage)
+            : base(Service, Context, preImage)
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the owner team.
+        /// </summary>
+        /// <returns></returns>
+        private Guid? GetApplication(Guid applicationId)
+        {
+            var query = new QueryExpression("team")
+            {
+                ColumnSet = new ColumnSet("teamid"),
+
+                Criteria = new FilterExpression(LogicalOperator.And)
+                {
+                    Conditions =
+                      {
+                        new ConditionExpression("businessunitid", ConditionOperator.Equal, this._Context.BusinessUnitId),
+                        new ConditionExpression("name", ConditionOperator.Like, "% Owner")
+                      }
+                }
+            };
+
+            EntityCollection results = _Service.RetrieveMultiple(query);
+
+            if (results.Entities.Count > 0)
+                return results.Entities[0].Id;
+
+            return null;
+        }
+    }
+}
