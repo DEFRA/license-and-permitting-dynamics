3d947367998db8c32caf0a7aba62372f79ddc6ec
Chris Jibb <christopher.jibb@civica.co.uk>
2018-03-06 15:11:31 +0000

    WE-956 - Schedule 5 and RFI template work. Also includes new projects for Common and NewtonSoft shared code.

--- Crm/LicensingandPermitting/Defra.Lp/Plugins/Common/ProxyClasses/FieldMetaData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Defra.Lp.Common.ProxyClasses
-{
-    public class FieldMetadata
-    {
-        public bool ManagedMetadataType { get; internal set; }
-        public string Name { get; internal set; }
-        public object Value { get; internal set; }
-    }
-}
