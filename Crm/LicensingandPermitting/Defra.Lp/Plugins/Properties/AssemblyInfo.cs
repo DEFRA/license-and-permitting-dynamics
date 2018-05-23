c184459e6a0ebfa94504f77ab7fc7ef92dca576f
Hugo Herrera <hugo_c_herrera@msn.com>
2018-05-23 13:39:44 +0100

    Updated Lp.Plugins assembly version number.

--- Crm/LicensingandPermitting/Defra.Lp/Plugins/Properties/AssemblyInfo.cs
+++ Crm/LicensingandPermitting/Defra.Lp/Plugins/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyVersion("1.1.75.35")]
-[assembly: AssemblyFileVersion("1.1.75.35")]
+[assembly: AssemblyVersion("1.1.75.36")]
+[assembly: AssemblyFileVersion("1.1.75.36")]
