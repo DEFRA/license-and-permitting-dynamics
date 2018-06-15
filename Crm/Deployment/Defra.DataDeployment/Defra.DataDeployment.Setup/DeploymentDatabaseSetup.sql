d37a22ec8fb543e9aa065d354ce69929b6ea76fd
Hugo Herrera <hugo_c_herrera@msn.com>
2018-06-15 18:42:01 +0100

    Prepared multi-environment email set-up for SSIS data package. Different environments can now have different email addresses, required as PreProd/Prod/QA have different team email addresses. Pending test on QA.

--- Crm/Deployment/Defra.DataDeployment/Defra.DataDeployment.Setup/DeploymentDatabaseSetup.sql
+++ Crm/Deployment/Defra.DataDeployment/Defra.DataDeployment.Setup/DeploymentDatabaseSetup.sql
@@ -1,4 +1,5 @@
-﻿create table srcbusinessunit
+﻿
+create table srcbusinessunit
 (
 businessunitid uniqueidentifier primary key not null,
 name nvarchar(200) not null,
@@ -9,7 +10,8 @@ create table destbusinessunit
 (
 businessunitid uniqueidentifier primary key not null,
 name nvarchar(200) not null,
-parentbusinessunitid uniqueidentifier
+parentbusinessunitid uniqueidentifier,
+[parentbusinessunitname] [nvarchar](200) NULL
 )
 
 create table srclpteams
@@ -76,3 +78,54 @@ create table destqueues
 	incomingemailstatus int null,
 	mailboxstatus int null
 )
+
+CREATE TABLE [dbo].[queueemails](
+	[queue_name] [nvarchar](300) NOT NULL,
+	[email] [nvarchar](100) NULL,
+	[environment] [nvarchar](300) NULL
+) ON [PRIMARY]
+GO
+
+
+
+
+
+
+delete from queueemails where environment is not null
+
+-- CoreLp
+insert into queueemails (queue_name, email, environment)
+SELECT queue_name, replace(email,'@environment-agency.gov.uk','@defradev.onmicrosoft.com'), 'ea-lp-dev-corelp'
+  from [dbo].queueemails
+  where environment is null
+
+-- Waste
+insert into queueemails (queue_name, email, environment)
+SELECT queue_name, replace(email,'@environment-agency.gov.uk','@defradev.onmicrosoft.com'), 'ea-lp-dev-waste'
+  from [dbo].queueemails
+  where environment is null
+  
+-- Master
+insert into queueemails (queue_name, email, environment)
+SELECT queue_name, replace(email,'@environment-agency.gov.uk','@defradev.onmicrosoft.com'), 'ea-lp-dev-master'
+  from [dbo].queueemails
+  where environment is null
+  
+-- QA
+insert into queueemails (queue_name, email, environment)
+SELECT queue_name, replace(email,'@environment-agency.gov.uk','@defradev.onmicrosoft.com'), 'ea-lp-qa-master'
+  from [dbo].queueemails
+  where environment is null
+  
+-- UAT
+insert into queueemails (queue_name, email, environment)
+SELECT queue_name, replace(email,'@environment-agency.gov.uk','@defra.onmicrosoft.com'), 'ea-lp-uat'
+  from [dbo].queueemails
+  where environment is null
+
+-- PROD
+insert into queueemails (queue_name, email, environment)
+SELECT queue_name, email, 'ea-lp-prod'
+  from [dbo].queueemails
+  where environment is null
+
