c4781baff360e2cc0549cb26a01d946da67d3fdf
Hugo Herrera <hugo_c_herrera@msn.com>
2018-07-23 11:01:53 +0100

    Added/fixed SubString code activity functionality, code activity now prevents errors, added test project for code activity SubString extension.

--- Crm/Core/Defra.Lp.Core/Core.Helpers/Extensions/StringExtensions.cs
+++ Crm/Core/Defra.Lp.Core/Core.Helpers/Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Xrm.Sdk;
-
+﻿// -----------------------------------------------
+// Helper extensions for string processing
+// -----------------------------------------------
 namespace Core.Helpers.Extensions
 {
     /// <summary>
@@ -25,6 +26,44 @@ namespace Core.Helpers.Extensions
 
             // Truncate
             return originalText.Substring(0, maxLength);
+        }
+
+        /// <summary>
+        /// Safely performs a susbtring, prevents exceptions when parameters exceed the text length
+        /// </summary>
+        /// <param name="text">Text to be processed</param>
+        /// <param name="start">Start index - 0 based</param>
+        /// <param name="length">Length of the substring</param>
+        /// <param name="lefttoright">True if left to right substring, otherwise false</param>
+        /// <returns>Processed string result</returns>
+        public static string SafeSubstring(this string text, int start, int length, bool lefttoright = true)
+        {
+
+            // Length or start are invalid or text is null
+            if (length <= 0 || start < 0 || text == null)
+            {
+                return string.Empty;
+            }
+
+            // Right to left?
+            if (!lefttoright)
+            {
+                start = text.Length - length - start;
+            }
+
+            // Start is greater than text length?
+            if (start >= text.Length)
+            {
+                return string.Empty;
+            }
+
+            // Length beyond text end?
+            if (start + length >= text.Length)
+            {
+                length = text.Length - start;
+            }
+
+            return text.Substring(start, length);
         }
     }
 }
