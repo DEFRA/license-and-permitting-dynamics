5bff53a3ac742205c6ef18141204bb716ca5e997
Chris Jibb <christopher.jibb@civica.co.uk>
2018-10-24 15:35:47 +0100

    Updating comments and variable naming

--- Crm/LicensingandPermitting/Defra.Lp/Lp.DataAccess/DataAccessContactDetail.cs
+++ Crm/LicensingandPermitting/Defra.Lp/Lp.DataAccess/DataAccessContactDetail.cs
@@ -1,35 +1,30 @@
-﻿using System.CodeDom.Compiler;
-using Core.Model.Entities;
+﻿using Core.Model.Entities;
 namespace Lp.DataAccess
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using Lp.Model.EarlyBound;
-    using Microsoft.Crm.Sdk;
     using Microsoft.Xrm.Sdk.Query;
-    using Lp.Model.Crm;
-    using Core.Helpers.Extensions;
     using Microsoft.Xrm.Sdk;
     public static class DataAccessContactDetail
     {
         /// <summary>
-        /// Returns all the locations and location details linked to the given application
+        /// Returns a reference to a single Contact Detail record for a given Application
+        /// and Type
         /// </summary>
         /// <param name="service">CRM Organisation Service</param>
         /// <param name="applicationId">Application being searched</param>
         /// <param name="contactDetailType">Contact detail type being searched</param>
-        /// <returns>List of Locations and location details</returns>
+        /// <returns>Contact Detail Entity Reference</returns>
         public static EntityReference GetContactDetail(IOrganizationService service, Guid? applicationId, int contactDetailType)
         {
             // Set-up Location Query
-            QueryExpression qEdefraLocation = new QueryExpression(defra_addressdetails.EntityLogicalName) { TopCount = 1 };
-            qEdefraLocation.Criteria.AddCondition(ContactDetail.State, ConditionOperator.Equal, (int)defra_addressdetailsState.Active);
+            QueryExpression qEdefraContactDetail = new QueryExpression(defra_addressdetails.EntityLogicalName) { TopCount = 1 };
+            qEdefraContactDetail.Criteria.AddCondition(ContactDetail.State, ConditionOperator.Equal, (int)defra_addressdetailsState.Active);
             // Application Locations?
-            qEdefraLocation.Criteria.AddCondition(ContactDetail.Application, ConditionOperator.Equal, applicationId);
-            qEdefraLocation.Criteria.AddCondition(ContactDetail.Type, ConditionOperator.Equal, contactDetailType);
+            qEdefraContactDetail.Criteria.AddCondition(ContactDetail.Application, ConditionOperator.Equal, applicationId);
+            qEdefraContactDetail.Criteria.AddCondition(ContactDetail.Type, ConditionOperator.Equal, contactDetailType);
             // Query CRM
-            EntityCollection result = service.RetrieveMultiple(qEdefraLocation);
+            EntityCollection result = service.RetrieveMultiple(qEdefraContactDetail);
             if (result?.Entities != null && result.Entities.Count > 0)
             {
                 return result.Entities[0].ToEntityReference();
