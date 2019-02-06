cd7b31546da8851bf4851c05ae5154e96c021207
Hugo Herrera <hugo.herrera.defra@gmail.com>
2019-02-06 11:36:13 +0000

    WE-1967 - Added tests from Amruta, included Waste model

--- Crm/WastePermits/Defra.Lp.WastePermits/UIAutomation/WastePermitTests.cs
+++ Crm/WastePermits/Defra.Lp.WastePermits/UIAutomation/WastePermitTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Security;
+using Microsoft.Dynamics365.UIAutomation.Api;
 using Microsoft.Dynamics365.UIAutomation.Browser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WastePermits.Model.EarlyBound;
 
 namespace WastePermits.UIAutomation
 {
@@ -202,6 +205,111 @@ namespace WastePermits.UIAutomation
                 xrmBrowser.Grid.OpenRecord(0);
                 xrmBrowser.ThinkTime(1000);
             }
+        }
+
+
+        [TestMethod]
+        public void UCITestCreateContact()
+        {
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, pscWasteUserName1, pscWastePassword1);
+                xrmBrowser.ThinkTime(500);
+                try { xrmBrowser.GuidedHelp.CloseGuidedHelp(); }
+                catch { }
+
+                xrmBrowser.ThinkTime(2000);
+                try
+                {
+                    xrmBrowser.Dialogs.CloseWarningDialog();
+                }
+                catch { }
+
+                xrmBrowser.Navigation.OpenSubArea("LP", "Contacts");
+                xrmBrowser.CommandBar.ClickCommand("New");
+                xrmBrowser.ThinkTime(10000);
+
+                var fields = new List<Field>
+                {
+                    new Field() {Id = Contact.Fields.FirstName, Value = GetRandomString(4,10)},
+                    new Field() {Id = Contact.Fields.LastName, Value = GetRandomString(4,10)}
+                };
+                xrmBrowser.Entity.SetValue(new CompositeControl() { Id = "fullname", Fields = fields });
+                xrmBrowser.Entity.SetValue("emailaddress1", GetRandomString(4, 10) + "@test.com");
+                xrmBrowser.Entity.SetValue("mobilephone", GetRandomNumber(10, 20));
+                xrmBrowser.Entity.SetValue(new OptionSet { Name = "preferredcontactmethodcode", Value = "Email" });
+
+                xrmBrowser.CommandBar.ClickCommand("Save");
+            }
+
+
+
+        }
+
+
+        public static string GetRandomString(int minLen, int maxLen)
+        {
+            char[] Alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz0123456789").ToCharArray();
+            Random m_randomInstance = new Random();
+            Object m_randLock = new object();
+            int alphabetLength = Alphabet.Length;
+            int stringLength;
+            lock (m_randLock) { stringLength = m_randomInstance.Next(minLen, maxLen); }
+            char[] str = new char[stringLength];
+            // max length of the randomizer array is 5
+            int randomizerLength = (stringLength > 5) ? 5 : stringLength;
+            int[] rndInts = new int[randomizerLength];
+            int[] rndIncrements = new int[randomizerLength];
+            // Prepare a "randomizing" array
+            for (int i = 0; i < randomizerLength; i++)
+            {
+                int rnd = m_randomInstance.Next(alphabetLength);
+                rndInts[i] = rnd;
+                rndIncrements[i] = rnd;
+            }
+            // Generate "random" string out of the alphabet used
+            for (int i = 0; i < stringLength; i++)
+            {
+                int indexRnd = i % randomizerLength;
+                int indexAlphabet = rndInts[indexRnd] % alphabetLength;
+                str[i] = Alphabet[indexAlphabet];
+                // Each rndInt "cycles" characters from the array, 
+                // so we have more or less random string as a result
+                rndInts[indexRnd] += rndIncrements[indexRnd];
+            }
+            return (new string(str));
+        }
+        public static string GetRandomNumber(int minLen, int maxLen)
+        {
+            char[] Alphabet = ("0123456789").ToCharArray();
+            Random m_randomInstance = new Random();
+            Object m_randLock = new object();
+            int alphabetLength = Alphabet.Length;
+            int stringLength;
+            lock (m_randLock) { stringLength = m_randomInstance.Next(minLen, maxLen); }
+            char[] str = new char[stringLength];
+            // max length of the randomizer array is 5
+            int randomizerLength = (stringLength > 5) ? 5 : stringLength;
+            int[] rndInts = new int[randomizerLength];
+            int[] rndIncrements = new int[randomizerLength];
+            // Prepare a "randomizing" array
+            for (int i = 0; i < randomizerLength; i++)
+            {
+                int rnd = m_randomInstance.Next(alphabetLength);
+                rndInts[i] = rnd;
+                rndIncrements[i] = rnd;
+            }
+            // Generate "random" string out of the alphabet used
+            for (int i = 0; i < stringLength; i++)
+            {
+                int indexRnd = i % randomizerLength;
+                int indexAlphabet = rndInts[indexRnd] % alphabetLength;
+                str[i] = Alphabet[indexAlphabet];
+                // Each rndInt "cycles" characters from the array, 
+                // so we have more or less random string as a result
+                rndInts[indexRnd] += rndIncrements[indexRnd];
+            }
+            return (new string(str));
         }
     }
 }
