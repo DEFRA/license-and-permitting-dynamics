89c81b9aaa894dc86988b2de12920f96eb03b8e0
Kassim Hassan <kassim@drd-uk.com>
2019-03-07 22:08:54 +0000

    revert attaempt

--- Crm/WastePermits/Defra.Lp.WastePermits/Solution/WebResources/defra_/addresslookup/postcodeOnchange.js
+++
@@ -1,8 +0,0 @@
-function postcodeOnChange() {
-   var addressWebResource = Xrm.Page.getControl("WebResource_address");
-   var src = addressWebResource.getSrc();
-   addressWebResource .setSrc(null)
-   addressWebResource .setSrc(src);
-  //  Xrm.Page.getControl("WebResource_address").setSrc(Xrm.Page.getControl("WebResource_address").getSrc());
-    Xrm.Page.getControl("WebResource_address").setVisible(true);
-}
